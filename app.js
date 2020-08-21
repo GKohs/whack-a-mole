f2ba428bb74352bee31a5cfd5c5d17bb69279289
Gunnar Kohs <gkohs@students.uni-mainz.de>
2020-08-21 08:42:14 +0200

    Added Strike - Counts how many consecutive times the player could hit. - changed mouseup to mousedown - removed picture when hit

--- app.js
+++ app.js
@@ -4,38 +4,70 @@
 const square = document.querySelectorAll('.square');
 const mole = document.querySelectorAll('.mole');
 const timeLeft = document.querySelector('#time-left');
-let score = document.querySelector('#score');
+
+let displayScore = document.querySelector('#score');
+let displayStrike = document.querySelector('#strike');
+let countStrike = 0;
+let isStrike = false;
+
+let moleDuration = 1000;
 
 let result = 0;
 let currentTime = timeLeft.textContent;
 
 function randomSquare () {
-  square.forEach(className => {
-    className.classList.remove('mole');
-  });
+  if (!isStrike) {
+    countStrike = 0;
+    displayStrike.textContent = countStrike;
+
+    square.forEach(className => {
+      className.classList.remove('mole');
+    });
+  }
+
+  isStrike = false;
+
   let randomPosition = square[Math.floor(Math.random() * 9)];
   randomPosition.classList.add('mole');
 
-  // assign the id of the randomPosition to hitPosition for later  use
+  // assign the id of the randomPosition to hitPosition for later use
   hitPosition = randomPosition.id;
 }
 
 square.forEach(id => {
-  id.addEventListener('mouseup', () => {
+  id.addEventListener('mousedown', () => {
     if (id.id === hitPosition) {
       result = result + 1;
-      score.textContent = result;
+      displayScore.textContent = result;
+      removeMole(id);
+
+      countStrike += 1;
+      displayStrike.textContent = countStrike;
     };
   });
 })
 
+// removes Mole
+function removeMole (id) {
+  hitPosition = 0
+  id.classList.remove('mole');
+  isStrike = true;
+};
+
+// timer to change mole
 function moveMole () {
-  let timerId = null;
-  timerId = setInterval(randomSquare, 1000);
+  var timerId = setInterval(randomSquare, moleDuration);
 }
 
-moveMole();
+// More about this topic:
+//  https://stackoverflow.com/questions/1280263/changing-the-interval-of-setinterval-while-its-running
+function changeMoleDuration (time) {
+  moleDuration += time;
+  clearInterval(timerId);
+  timerId = setInterval(randomSquare, moleDuration);
+}
 
+// counts to 0, shows alert
 function countDown () {
   currentTime--;
   timeLeft.textContent = currentTime;
@@ -45,5 +77,7 @@ function countDown () {
     alert('GAME OVER! Your final score is' + result);
   }
 }
+
+moveMole();
 
 let timerId = setInterval(countDown, 1000);
